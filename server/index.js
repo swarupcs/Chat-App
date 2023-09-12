6ae3120098f5ff2ee96ad682e69abb85d11413fc
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-12 22:12:28 +0530

    setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+const { log } = require("console");
+
+const app = express();
+const port = 4500 || process.env.PORT;
+
+app.get("/",(req,res)=> {
+    res.send("Server is Working")
+})
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on("connection", ()=> {
+    console.log("New Connection");
+})
+
+server.listen(port, ()=> {
+    console.log(`server is working on http://localhost:${port}`);
+})
