d8dd83ae962986bfd050384b7f4aefd5a680ce99
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-12 22:52:15 +0530

    1st commit

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
-const { log } = require("console");
+
 
 const app = express();
 const port = 4500 || process.env.PORT;
 
+app.use(cors());
 app.get("/",(req,res)=> {
     res.send("Server is Working")
 })
