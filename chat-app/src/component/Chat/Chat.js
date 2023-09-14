3e8ee2b02b239d74a0f26fcb1c49758102093fbf
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-14 22:24:01 +0530

    Complete

--- chat-app/src/component/Chat/Chat.js
+++ chat-app/src/component/Chat/Chat.js
@@ -1,49 +1,87 @@
-import React, { useEffect } from 'react'
-import {user} from "../Join/Join";
-import socketIo from "socket.io-client"
-import "./Chat.css"
+import React, { useEffect, useState } from 'react'
+import { user } from "../Join/Join";
+import socketIo from "socket.io-client";
+import "./Chat.css";
 import sendLogo from "../../images/send.png";
+import Message from "../Message/Message";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import closeIcon from "../../images/closeIcon.png";
+
+let socket;
 
 const ENDPOINT = "http://localhost:4500/";
 
 const Chat = () => {
+    const [id, setid] = useState("");
+    const [messages, setMessages] = useState([])
 
-const socket = socketIo(ENDPOINT, {transports : ['websocket']});
+    const send = () => {
+        const message = document.getElementById('chatInput').value;
+        socket.emit('message', { message, id });
+        document.getElementById('chatInput').value = "";
+    }
 
-useEffect(() => {
-  socket.on('connect', () => {
-    // alert("Connected");
-  })
+    console.log(messages);
+    useEffect(() => {
+        socket = socketIo(ENDPOINT, { transports: ['websocket'] });
 
-  socket.emit('joined', {user})
+        socket.on('connect', () => {
+            alert('Connected');
+            setid(socket.id);
 
-  socket.on('welcome',(data)=> {
-    console.log(data.user, data.message);
-  })
+        })
+        console.log(socket);
+        socket.emit('joined', { user })
 
-  socket.on('userJoined', (data)=> {
-    console.log(data.user, data.message);
-  })
+        socket.on('welcome', (data) => {
+            setMessages([...messages, data]);
+            console.log(data.user, data.message);
+        })
 
+        socket.on('userJoined', (data) => {
+            setMessages([...messages, data]);
+            console.log(data.user, data.message);
+        })
 
-  return () => {
-    
-  }
-}, [])
+        socket.on('leave', (data) => {
+            setMessages([...messages, data]);
+            console.log(data.user, data.message)
+        })
 
-  return (
-    <div className='chatPage'>
-      <div className='chatContainer'>
-        <div className='header'></div>
-            <div className='chatBox'></div>
-            <div className='inputBox'>
-            <input type="text" id='chatInput' />
-            <button className='sendBtn'><img src={sendLogo} alt="Send" /></button>
+        return () => {
+            socket.emit('disconnected');
+            socket.off();
+        }
+    }, [])
+
+    useEffect(() => {
+        socket.on('sendMessage', (data) => {
+            setMessages([...messages, data]);
+            console.log(data.user, data.message, data.id);
+        })
+        return () => {
+            socket.off();
+        }
+    }, [messages])
+
+    return (
+        <div className="chatPage">
+            <div className="chatContainer">
+                <div className="header">
+                    <h2>C CHAT</h2>
+                    <a href="/"> <img src={closeIcon} alt="Close" /></a>
+                </div>
+                <ReactScrollToBottom className="chatBox">
+                    {messages.map((item, i) => <Message user={item.id === id ? '' : item.user} message={item.message} classs={item.id === id ? 'right' : 'left'} />)}
+                </ReactScrollToBottom>
+                <div className="inputBox">
+                    <input onKeyPress={(event) => event.key === 'Enter' ? send() : null} type="text" id="chatInput" />
+                    <button onClick={send} className="sendBtn"><img src={sendLogo} alt="Send" /></button>
+                </div>
             </div>
-        
-      </div>
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default Chat
