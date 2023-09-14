a11aa7cb30d9975d0c47dccfae7e2113380122c6
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-14 15:32:46 +0530

    user join complete

--- chat-app/src/component/Chat/Chat.js
+++ chat-app/src/component/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import {user} from "../Join/Join";
 import socketIo from "socket.io-client"
+import "./Chat.css"
+import sendLogo from "../../images/send.png";
 
 const ENDPOINT = "http://localhost:4500/";
 
@@ -10,22 +12,35 @@ const socket = socketIo(ENDPOINT, {transports : ['websocket']});
 
 useEffect(() => {
   socket.on('connect', () => {
-    alert("connected");
+    // alert("Connected");
   })
+
+  socket.emit('joined', {user})
+
+  socket.on('welcome',(data)=> {
+    console.log(data.user, data.message);
+  })
+
+  socket.on('userJoined', (data)=> {
+    console.log(data.user, data.message);
+  })
+
 
   return () => {
     
   }
-}, [socket])
+}, [])
 
   return (
     <div className='chatPage'>
       <div className='chatContainer'>
-        <div className='header'>
+        <div className='header'></div>
             <div className='chatBox'></div>
-            <div className='inputBox'></div>
-
-        </div>
+            <div className='inputBox'>
+            <input type="text" id='chatInput' />
+            <button className='sendBtn'><img src={sendLogo} alt="Send" /></button>
+            </div>
+        
       </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const socketIO = require("socket.io");
 const app = express();
 const port = 4500 || process.env.PORT;
 
+const users = [{}];
+
 app.use(cors());
 app.get("/",(req,res)=> {
     res.send("Server is Working")
@@ -16,9 +18,18 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-io.on("connection", ()=> {
+io.on("connection", (socket)=> {
     console.log("New Connection");
-})
+
+    socket.on('joined', ({user})=> {
+        users[socket.id] = user;
+        console.log(`${user} has joined`);
+        socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined`})
+    })
+
+    socket.emit('welcome', {user:'Admin', message: `Welcome to the chat`})
+
+});
 
 server.listen(port, ()=> {
     console.log(`server is working on http://localhost:${port}`);
